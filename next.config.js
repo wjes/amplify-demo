690506d7cc451015fb19f7a58621e6fd19b5c23d
Warren Eaton <warren.eaton.sciaccaluga@gmail.com>
2023-08-01 13:58:01 -0400

    First demo

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config, { webpack, isServer, nextRuntime }) => {
+    // Avoid AWS SDK Node.js require issue
+    if (isServer && nextRuntime === "nodejs")
+      config.plugins.push(
+        new webpack.IgnorePlugin({ resourceRegExp: /^aws-crt$/ })
+      );
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
